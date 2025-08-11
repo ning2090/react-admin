4b546fcec98586861182b215547f084b041f8b7e
wujianing <1085838220@qq.com>
2025-08-11 13:16:01 +0800

    add

--- src/components/commonTag/index.js
+++ src/components/commonTag/index.js
@@ -1,6 +1,6 @@
-import { Tag, Space } from "antd";
+import { Tag, Space, Dropdown } from "antd";
 import { useSelector, useDispatch } from "react-redux";
-import { closeTab, setCurrentMenu } from '../../store/reducers/tab'
+import { closeTab, setCurrentMenu, closeOtherTabs, closeAllTabs } from '../../store/reducers/tab'
 import { useLocation, useNavigate } from "react-router-dom";
 import './index.css'
 
@@ -8,54 +8,108 @@ const CommonTag = () => {
     const tabList = useSelector(state => state.tab.tabList)
     const currentMenu = useSelector(state => state.tab.currentMenu)
     const dispatch = useDispatch()
-    const action = useLocation()
+    const location = useLocation()
     const navigate = useNavigate()
+
+    // 关闭标签
     const handleClose = (tag, index) => {
-        let length = tabList.length - 1 
-        dispatch(closeTab(tag))
-        // 关闭的不是当前的tag
-        if (tag.path !== action.pathname){
+        // 不能关闭最后一个标签
+        if (tabList.length === 1) {
             return
         }
-        if (index){
-            const curData = tabList[index - 1]
-            dispatch(setCurrentMenu(curData))
-            navigate(curData.path)
-        }else{
-            // 如果tags至少存在一个数据，则选择后一个tag
-            if(tabList.length > 1){
-                const nextData = tabList[index + 1]
-                dispatch(setCurrentMenu(nextData))
-                navigate(nextData.path)
+
+        dispatch(closeTab(tag))
+        
+        // 如果关闭的是当前标签，需要跳转到其他标签
+        if (tag.path === location.pathname) {
+            let targetTab
+            if (index === tabList.length - 1) {
+                // 关闭的是最后一个，跳转到前一个
+                targetTab = tabList[index - 1]
+            } else {
+                // 关闭的不是最后一个，跳转到后一个
+                targetTab = tabList[index + 1]
             }
+            dispatch(setCurrentMenu(targetTab))
+            navigate(targetTab.path)
         }
     }
 
-    // 点击tag
+    // 点击标签
     const handleChange = (tag) => {
         dispatch(setCurrentMenu(tag))
         navigate(tag.path)
     }
-    // tag的显示
-    const setTag = (flag, item, index) => {
+
+    // 右键菜单项
+    const getContextMenuItems = (tag, index) => {
+        const items = [
+            {
+                key: 'refresh',
+                label: '刷新页面',
+                onClick: () => {
+                    window.location.reload()
+                }
+            },
+            {
+                key: 'close',
+                label: '关闭标签',
+                disabled: tabList.length === 1,
+                onClick: () => handleClose(tag, index)
+            },
+            {
+                key: 'closeOthers',
+                label: '关闭其他标签',
+                disabled: tabList.length === 1,
+                onClick: () => {
+                    dispatch(closeOtherTabs(tag))
+                    navigate(tag.path)
+                }
+            },
+            {
+                key: 'closeAll',
+                label: '关闭所有标签',
+                onClick: () => {
+                    dispatch(closeAllTabs())
+                    navigate('/home')
+                }
+            }
+        ]
+        return items
+    }
+
+    // 渲染标签
+    const renderTag = (item, index) => {
+        const isActive = item.path === currentMenu.path
+        
         return (
-            flag ?
-            <Tag color="#55acee" closeIcon onClose={() => handleClose(item, index)} key={item.name}>{item.label}</Tag>    
-            :
-            <Tag onClick={() => handleChange(item)} key={item.name}>{item.label}</Tag>      
+            <Dropdown
+                key={item.name}
+                menu={{ items: getContextMenuItems(item, index) }}
+                trigger={['contextMenu']}
+            >
+                <Tag
+                    className={`tab-tag ${isActive ? 'active' : ''}`}
+                    color={isActive ? '#1890ff' : undefined}
+                    closable={tabList.length > 1}
+                    onClose={(e) => {
+                        e.preventDefault()
+                        handleClose(item, index)
+                    }}
+                    onClick={() => handleChange(item)}
+                >
+                    {item.label}
+                </Tag>
+            </Dropdown>
         )
     }
 
     return (
-        <Space className="common-tag" size={[0-8]} wrap>
-            {/* <Tag>首页</Tag>
-            <Tag color="#55acee" closeIcon onClose={() => handleClose()}>
-                用户列表
-            </Tag> */}
-            {
-                currentMenu.name && tabList.map((item, index) => (setTag(item.path === currentMenu.path, item, index)))
-            }
-        </Space>
+        <div className="common-tag">
+            <Space size={[0, 8]} wrap>
+                {tabList.map((item, index) => renderTag(item, index))}
+            </Space>
+        </div>
     )
 }
 
--- src/store/reducers/tab.js
+++ src/store/reducers/tab.js
@@ -6,42 +6,60 @@ const tabSlice = createSlice({
         isCollapsed: false,
         tabList: [
             {
-                path: '/',
+                path: '/home',
                 name: 'home',
                 label: '首页'
             }
         ],
-        currentMenu: {}
+        currentMenu: {
+            path: '/home',
+            name: 'home',
+            label: '首页'
+        }
     },
     reducers: {
         collapseMenu: state => {
             state.isCollapsed = !state.isCollapsed
         },
         selectMenuList: (state, {payload:val}) => {
-            if(val.name !== '/home') {
+            if(val.name !== 'home') {
                 state.currentMenu = val
                 // 已存在的tag不需要添加
                 const result = state.tabList.findIndex(item => item.name === val.name)
                 if (result === -1){
                     state.tabList.push(val)
                 }
-            }else if(val.name === 'home' && state.tabList.length === 1){
-                state.currentMenu = {}
+            } else if(val.name === 'home') {
+                state.currentMenu = val
             }
         },
         closeTab: (state,{payload:val}) => {
             let res = state.tabList.findIndex(item => item.name === val.name)
-            state.tabList.splice(res, 1)
+            if (res !== -1) {
+                state.tabList.splice(res, 1)
+            }
         },
         setCurrentMenu:(state, {payload:val}) =>{
-            if(val.name === 'home'){
-                state.currentMenu = {}
-            }else{
-                state.currentMenu = val
+            state.currentMenu = val
+        },
+        closeOtherTabs: (state, {payload: val}) => {
+            state.tabList = [val]
+            state.currentMenu = val
+        },
+        closeAllTabs: (state) => {
+            state.tabList = [{
+                path: '/home',
+                name: 'home',
+                label: '首页'
+            }]
+            state.currentMenu = {
+                path: '/home',
+                name: 'home',
+                label: '首页'
             }
         }
     }
 })
 
-export const { collapseMenu, selectMenuList, closeTab, setCurrentMenu } = tabSlice.actions
+export const { collapseMenu, selectMenuList, closeTab, setCurrentMenu, closeOtherTabs, closeAllTabs } = tabSlice.actions
 export default tabSlice.reducer
